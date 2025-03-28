81ac7b916d35334f21c1dc5681992dfb58e2fcf5
Mauricio <152633503+mauriciobalbuena@users.noreply.github.com>
2025-03-28 15:44:42 +0000

    first

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { Tarjeta } from './Tarjeta'
 export function App() {
   return (
     <div>
-        <div>Personajes de Mario Bros</div>
-        <Tarjeta/>
-        <Tarjeta/>
-        <Tarjeta/>
+        <div>Personajes de Mario Bros que mas me gustan</div>
+        <Tarjeta nombre="mario" colorfavorito="rojo"/>
+        <Tarjeta nombre="luigi" colorfavorito="verde"/>
+        <Tarjeta nombre="peaches" colorfavorito="durazno"/>
     </div>
   )
 }
